844a22c370bd7156e6914f12a46403aad5bfa52a
Yoga Permana <yoga@xcidic.com>
2021-03-24 20:17:08 +0700

    slicing home page / migrasi component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import Arrived from './components/Arrived';
+import AsideMenu from './components/AsideMenu';
+import Browse from './components/Browse';
+import Clients from './components/Clients';
+import Footer from './components/Footer';
+import Header from './components/header';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-lg">
-      <div className="ml-6 pt-1">
-        <h1 className="pt-1">
-          Tailwind and Create React App
-        </h1>
-        <p className="text-base text-gray-700 leading-normal">
-          Building Apps together
-        </p>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+    </>
   );
 }
 
